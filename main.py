45c5a814dec346537850946df6c6d6290c103743
m.r.sadeghi67@gmail.com <m.r.sadeghi67@gmail.com>
2022-11-07 18:03:21 +0100

    main.py: result format is changed

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import os
 from webcam import get_image_from_webcam
 from digits import get_digits
 from mnistNet import predict_digits,load_trained_net, do_train
 
 
 # do_train()
-# load_trained_net()
-imageName = get_image_from_webcam()
-# imageName = 'webcam/webcammkhsgqtdzv.png'
-# images = get_digits(imageName)
-# val1 = predict_digits(images)
-# print(*val1)
+load_trained_net()
+# imageName = get_image_from_webcam()
+imageName = 'webcam/webcammkhsgqtdzv.png'
+images = get_digits(imageName)
+val = predict_digits(images)
+clear = lambda: os.system('cls')
+clear()
+val_str = ''.join(map(str, val))
+print('Predicted value is : '+ str(''.join(val_str)))
